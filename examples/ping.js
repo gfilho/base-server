5eff4e82c97000b33d749f5cce5be13fd8083aa2
Gustavo S. S. Filho <gustavossfilho@gmail.com>
2019-04-28 16:29:17 -0300

    Add example of code using @gfilho/base-server

---
+++ examples/ping.js
@@ -0,0 +1,21 @@
+// Include base-server
+const Server = require('../');
+
+// Configuration of server
+const config = {
+  router: {
+    address: 'api',
+    port: 1234,
+  },
+};
+
+// Instance of Server
+const server = new Server(config);
+
+// Bind requests
+server.get('/ping', (req, res) => {
+  res.json({ msg: 'Server is alive' });
+});
+
+// Run Server
+server.run();
